49086160bb0fd8520ad40973ac29cb469ef53913
rpereyra2 <rpereyra2@student.ccc.edu>
2026-07-24 18:07:35 -0500

    Add files via upload

---
+++ pereyra_r_lab3-2/script.js
@@ -0,0 +1,34 @@
+/*
+    Applied Lab 3-2
+    Ducky World JavaScript
+*/
+
+/*
+    Displays a welcome message.
+*/
+function welcomeCustomer() {
+
+}
+
+/*
+    Prompts the user for their name and displays a greeting.
+*/
+function greetCustomer() {
+
+}
+
+/*
+    Calculates and displays the total number of ducks ordered.
+*/
+function calculateTotalDucks() {
+
+}
+
+/*
+    Validates the order before submitting the form.
+    Return true to allow submission.
+    Return false to prevent submission.
+*/
+function validateOrder() {
+
+}
